d038d320bd3b04015c6728361161ae6216c0f9d6
alan-anaya <58603905+alan-anaya@users.noreply.github.com>
2020-09-04 03:22:26 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,26 +4,7 @@ provider "aws" {
   secret_key = "GMVEWIgYUdYQwl/WajLPRbe6zVzyUxedZOol1gqA"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-}
-
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+resource "aws_instance" "example" {
+  ami = "ami-098f16afa9edf40be"
   instance_type = "t2.micro"
-}
-output "ip"{
-value= "${aws_instance.web.public_ip}"
 }
