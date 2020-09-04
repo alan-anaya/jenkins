ede7282285200dd8be3a5531c2acbac8eff57940
alan-anaya <58603905+alan-anaya@users.noreply.github.com>
2020-09-04 10:51:54 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,6 +7,6 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-098f16afa9edf40be"
   instance_type = "t2.micro"
-  vpc_security_group_ids=["sg-0641c44891df5cdd7"]
-  subnet_id="subnet-0013ee5b1622df84a"
+  vpc_security_group_ids=["sg-0577bcc94d3bf8c22"]
+  subnet_id="subnet-027f34ac668d76875"
 }
