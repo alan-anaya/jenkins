956b409dace8964596ee45dea0c9035d88b803f9
alan-anaya <58603905+alan-anaya@users.noreply.github.com>
2020-09-04 04:01:26 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA6RDQW5NPBIEFW7I2"
-  secret_key = "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"
+  access_key = "AKIA34ECHCUI6W5PGIW5"
+  secret_key = "/d8BiHgsuORnnv1krhqtXwVbQOImEtjcx1Z2xnr9"
 }
 
 resource "aws_instance" "example" {
   ami = "ami-098f16afa9edf40be"
   instance_type = "t2.micro"
-  vpc_security_group_ids=["sg-0b8932938d5e79914"]
-  subnet_id="subnet-00149bfbddb8f6d5e"
+  vpc_security_group_ids=["sg-0641c44891df5cdd7"]
+  subnet_id="subnet-0013ee5b1622df84a"
 }
